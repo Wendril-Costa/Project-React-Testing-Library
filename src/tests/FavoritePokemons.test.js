c2e4561c5ba3f80479602cf4eb46d31aae6507c9
wendril <wendrild.c.n@gmail.com>
2022-04-19 12:31:07 -0300

    Requisito 3 - Feito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente FaoritePokemons', () => {
+  it('Teste se é exibido na tela a mensagem `No favorite pokemon found`', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const favorite = screen.queryAllByText('id');
+    const pokémonsFavorite = favorite.length;
+    expect(favorite).toHaveLength(pokémonsFavorite);
+  });
+});
